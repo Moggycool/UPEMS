27ebf7ef34349f1d0e22036a4f836e19fd7639e9
Moggycool <mogycool@gmail.com>
2026-04-08 17:12:01 +0300

    Update URL configuration for appraisals and modify redirect path

--- performance_mgmt/urls.py
+++ performance_mgmt/urls.py
@@ -1,13 +1,15 @@
 """URL configuration for performance_mgmt project."""
 from django.contrib import admin
-from django.views.generic import RedirectView
 from django.urls import include, path
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("", RedirectView.as_view(url="/accounts/", permanent=False)),
+    path("", RedirectView.as_view(url="/my/", permanent=False)),
     path("admin/", admin.site.urls),
     path("accounts/", include("apps.accounts.urls")),
-    path("appraisals/", include("apps.appraisals.urls")),
+
+    # keep only this one
+    path("", include("apps.appraisals.urls")),
+
     path("reports/", include("apps.reports.urls")),
 ]
